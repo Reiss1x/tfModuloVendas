693de7c2ec709f020f9b26dd6eab702cd69e3e03
GABRIEL REIS <GAB08@JFRS.LOCAL>
2023-10-19 14:14:47 -0300

    Projeto inicial

---
+++ src/main/java/com/pucrs/modulovendas/ModuloVendasApplication.java
@@ -0,0 +1,13 @@
+package com.pucrs.modulovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloVendasApplication.class, args);
+	}
+
+}

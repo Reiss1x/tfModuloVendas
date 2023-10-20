719da2d951620eeb0b089494ce9d458ef2ea6bf7
GABRIEL REIS <GAB08@JFRS.LOCAL>
2023-10-20 15:04:06 -0300

    Implementação das classes necessárias para os endpoints de solicitação e efetivação de orcamentos

--- src/main/java/com/pucrs/modulovendas/domain/VendasService.java
+++ src/main/java/com/pucrs/modulovendas/domain/VendasService.java
@@ -2,6 +2,7 @@ package com.pucrs.modulovendas.domain;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +27,15 @@ public class VendasService {
     public List<Pedido> getPedidos() {
         return op.getPedidos();
     }
-    
+
+    public List<Orcamento> getOrcamentos() {
+        return op.getOrcamentos();
+    }
+
+    public Orcamento getOrcamento(int orcamentoId) {
+        return op.getOrcamentoById(orcamentoId);
+    }
+
     public Orcamento postOrcamento(Pedido ped, int data,List<Produto> produtos){
         int somatorio = 0;
         for(Item item : ped.getListaProd()){
@@ -41,5 +50,31 @@ public class VendasService {
         return orc;
     }
 
-    
+    public Orcamento efetivarOrcamento(int orcamentoId, List<Produto> produtos){
+        Orcamento orc = op.getOrcamentoById(orcamentoId);
+        Pedido p = orc.getPedido();
+        for (Item item : p.getListaProd()){
+            Optional<Produto> aux = produtos.stream().filter(i -> i.getCod() == item.getProdId()).findFirst();
+            if(aux.isPresent()){
+                Produto prod = aux.get();
+                if(!(prod.getQnt() >= item.getQuantidade())){
+                throw new IllegalArgumentException("Produto: " + prod.getDesc()+ " sem estoque. Orçamento não efetivado.");
+            }
+            }
+            
+        }
+        for (Item item : p.getListaProd()){
+            Optional<Produto> aux = produtos.stream().filter(i -> i.getCod() == item.getProdId()).findFirst();
+            if(aux.isPresent()){
+                Produto prod = aux.get();
+                prod.setQnt(prod.getQnt() - item.getQuantidade());
+            }    
+        }
+        orc.setEfetivado();
+        return orc;
+    }
+
+    public List<Orcamento> getRelatorio() {
+        return op.getOrcamentos().stream().filter(x -> x.getEfetivado() == true).collect(Collectors.toList());
+    }
 }
--- src/main/java/com/pucrs/modulovendas/interfaces/LojaController.java
+++ src/main/java/com/pucrs/modulovendas/interfaces/LojaController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pucrs.modulovendas.domain.EstoqueService;
 import com.pucrs.modulovendas.domain.VendasService;
+import com.pucrs.modulovendas.entities.Orcamento;
 import com.pucrs.modulovendas.entities.Pedido;
 import com.pucrs.modulovendas.entities.Produto;
 
@@ -50,10 +53,35 @@ public class LojaController {
         }
         return pedidos;
     }
-    
+
     //visualizar pedidos
     @GetMapping("/admin/pedidos")
     public List<Pedido> getPedidos(){
         return vs.getPedidos();
+    }
+
+    //solicitar orcamento específico (visualizar)
+    @GetMapping("/admin/pedidos/{pedidoId}/solicitarOrcamento")
+    public String postSolicitarOrcamento(@PathVariable int pedidoId){
+        return vs.getOrcamento(pedidoId).toString();
+    }
+
+    //visualizar orcamentos não efetivados
+    @GetMapping("/admin/orcamentos")
+    public List<Orcamento> getOrcamentos(){
+        return vs.getOrcamentos();
+    } 
+
+    //efetuar compra
+    @GetMapping("/admin/orcamentos/{orcamentoId}/efetivar")
+    public Orcamento efetivarOrcamento(@PathVariable int orcamentoId){
+        List<Produto> listaProd = es.getProds();
+        return vs.efetivarOrcamento(orcamentoId, listaProd);
+    }
+
+    //get relatorios
+    @GetMapping("/admin/relatorio")
+    public List<Orcamento> getRelatorio(@RequestParam int num){
+        return vs.getRelatorio().subList(0, num);
     }
 }
